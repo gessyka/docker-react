befe569b044d1a37b1c9e8e8e33b8aeead891e63
Jessica Hagerman <jhagerman@gmail.com>
2022-03-12 21:34:52 -0500

    new message in app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Your Mom.
+          Brie is cute.
         </p>
         <a
           className="App-link"
